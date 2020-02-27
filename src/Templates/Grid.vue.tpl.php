2fe2c46bc6d57b1e3b83ca83f393fe73804a7902
Paul Barham <paulb@pdblaptop.local>
2020-02-26 21:13:49 -0600

    Now grid filter area expanded from 8 to 12 columns

--- src/Templates/Grid.vue.tpl.php
+++ src/Templates/Grid.vue.tpl.php
@@ -8,7 +8,7 @@
         </div>
         <!-- Grid Actions Top -->
         <div class="grid-top row mb-0 align-items-center">
-            <div class="col-lg-8 mb-2">
+            <div class="col-lg-12 mb-2">
                 <form class="form-inline mb-0">
                     <a v-if="params.CanAdd" href="#" @click.default="goToNew" class="btn btn-primary mb-3 mb-sm-2 mr-3">Add a
                         [[display_name_singular]]</a>
