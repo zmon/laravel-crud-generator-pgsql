b5c276769f5f57bdd1ded8841c74968d7b2db940
Paul Barham <paulb@pdblaptop.local>
2020-02-26 18:45:53 -0600

    Resolved issue where Print PDF button does not show, and indented

--- src/Templates/view.index.tpl.php
+++ src/Templates/view.index.tpl.php
@@ -4,21 +4,22 @@
 @section('page-header-title', '[[display_name_plural]]')
 @section('page-help-link', '#TODO')
 @section('page-header-breadcrumbs')
-<ol class="breadcrumb">
-    <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
-    <li class="breadcrumb-item active" aria-current="location">[[display_name_plural]]</li>
-</ol>
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
+        <li class="breadcrumb-item active" aria-current="location">[[display_name_plural]]</li>
+    </ol>
 @endsection
 @section('content')
-<[[view_folder]]-grid :params="{
-Page: '{{ $page }}',
-Search: '{{ $search }}',
-sortOrder: '{{ $direction }}',
-sortKey: '{{ $column }}',
-CanAdd: '{{ $can_add }}',
-CanEdit: '{{ $can_edit }}',
-CanShow: '{{ $can_show }}',
-CanDelete: '{{ $can_delete }}',
-CanExcel: '{{ $can_excel }}'
-}"></[[view_folder]]-grid>
+    <[[view_folder]]-grid :params="{
+        Page: '{{ $page }}',
+        Search: '{{ $search }}',
+        sortOrder: '{{ $direction }}',
+        sortKey: '{{ $column }}',
+        CanAdd: '{{ $can_add }}',
+        CanEdit: '{{ $can_edit }}',
+        CanShow: '{{ $can_show }}',
+        CanDelete: '{{ $can_delete }}',
+        CanExcel: '{{ $can_excel }}',
+        CanPdf: '{{ $can_pdf }}'
+    }"></[[view_folder]]-grid>
 @endsection
